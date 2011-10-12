498d0c1b982b4c90f0d254ba19c81415ed3027d4
David Mitchell <davem@iabyn.com>
2011-10-12 14:01:50 +0100

    make qr/(?{})/ behave with closures
    With this commit, qr// with a literal (compile-time) code block
    will Do the Right Thing as regards closures and the scope of lexical vars;
    in particular, the following now creates three regexes that match 1, 2
    and 3:
    
        for my $i (0..2) {
            push @r, qr/^(??{$i})$/;
        }
        "1" =~ $r[1]; # matches
    
    Previously, $i would be evaluated as undef in all 3 patterns.
    
    This is achieved by wrapping the compilation of the pattern within a
    new anonymous CV, which is then attached to the pattern. At run-time
    pp_qr() clones the CV as well as copying the REGEXP; and when the code
    block is executed, it does so using the pad of the cloned CV.
    Which makes everything come out all right in the wash.
    
    The CV is stored in a new field of the REGEXP, called qr_anoncv.
    
    Note that run-time qr//s are still not fixed, e.g. qr/$foo(?{...})/;
    nor is it yet fixed where the qr// is embedded within another pattern:
    continuing with the code example from above,
    
        my $i = 99;
        "1"   =~ $r[1];    # bare qr:     matches: correct!
        "X99" =~ /X$r[1]/; # embedded qr: matches: whoops, it's still
    					       seeing the wrong $i

--- regcomp.c
+++ regcomp.c
@@ -7329,7 +7329,8 @@ S_reg(pTHX_ RExC_state_t *pRExC_state, I32 paren, I32 *flagp,U32 depth)
 			{
 			    o = o->op_sibling;
 			}
-			n = add_data(pRExC_state, 1, "l");
+			n = add_data(pRExC_state, 1,
+				   (RExC_flags & PMf_HAS_CV) ? "L" : "l");
 			RExC_rxi->data->data[n] = (void*)o->op_next;
 			pRExC_state->next_code_or_const = o->op_sibling;
 		    }
@@ -11907,6 +11908,7 @@ Perl_pregfree2(pTHX_ REGEXP *rx)
     SvREFCNT_dec(r->saved_copy);
 #endif
     Safefree(r->offs);
+    SvREFCNT_dec(r->qr_anoncv);
 }
 
 /*  reg_temp_copy()
@@ -11970,6 +11972,7 @@ Perl_reg_temp_copy (pTHX_ REGEXP *ret_x, REGEXP *rx)
     ret->saved_copy = NULL;
 #endif
     ret->mother_re = rx;
+    SvREFCNT_inc_void(ret->qr_anoncv);
     
     return ret_x;
 }
@@ -12051,6 +12054,7 @@ Perl_regfree_internal(pTHX_ REGEXP * const rx)
 		new_comppad = NULL;
 		break;
 	    case 'l':
+	    case 'L':
 	    case 'n':
 	        break;
             case 'T':	        
@@ -12179,6 +12183,7 @@ Perl_re_dup_guts(pTHX_ const REGEXP *sstr, REGEXP *dstr, CLONE_PARAMS *param)
     }
 
     RXp_PAREN_NAMES(ret) = hv_dup_inc(RXp_PAREN_NAMES(ret), param);
+    ret->qr_anoncv = MUTABLE_CV(sv_dup_inc((const SV *)ret->qr_anoncv, param));
 
     if (ret->pprivate)
 	RXi_SET(ret,CALLREGDUPE_PVT(dstr,param));
@@ -12291,6 +12296,7 @@ Perl_regdupe_internal(pTHX_ REGEXP * const rx, CLONE_PARAMS *param)
 		OP_REFCNT_UNLOCK;
 		/* Fall through */
 	    case 'l':
+	    case 'L':
 	    case 'n':
 		d->data[i] = ri->data->data[i];
 		break;
